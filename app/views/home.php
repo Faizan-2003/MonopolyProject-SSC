3b794a949016686c43247dbd623efb52cbc62231
ayazspv <ayazsaadatpour2@gmail.com>
2024-02-12 21:12:43 +0100

    Home page layout

--- app/views/home.php
+++ app/views/home.php
@@ -12,6 +12,22 @@
 
 <body>
     <h1>Welcome to Monopoly Inholland</h1>
+    <h2>Name: </h2>
+    <h2>Amount Money: $---</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Your Properties</th>
+                <th>Property Cost</th>
+                <th>Property fine</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td>Name Property</td>
+            <td>$$$</td>
+            <td>$$$</td>
+        </tbody>
+    </table>
 </body>
 
 </html>
