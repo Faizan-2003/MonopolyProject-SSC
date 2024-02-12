409bac8e5a199369a107557a159b4585327c97ed
ayazspv <ayazsaadatpour2@gmail.com>
2024-02-12 20:47:20 +0100

    Add User Page

---
+++ app/views/AdminPortal.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Monopoly Inholland</title>
+    <style>
+        <?php include __DIR__ . '/../public/CSS/Style.css'; ?>
+    </style>
+</head>
+
+<body>
+    <h1>Welcome to Monopoly Inholland Admin Portal</h1>
+    <a href="AddUser.php">Add User Page</a>
+</body>
+
+</html>
